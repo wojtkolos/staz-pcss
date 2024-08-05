bc8dfad77210bbca43709eb3dfdb0a96e9d37011
JakubPowezowski <jakub.powezowski4@wp.pl>
2024-08-05 12:20:16 +0200

    Add files via upload

--- skrypt.js
+++ skrypt.js
@@ -1,109 +1,42 @@
+function sprawdzPole(idPola, idKomunikatu) {
+    const pole = document.getElementById(idPola);
+    const wartosc = pole.value.trim();
 
-function sprawdz1() {
-
-    var imie = document.getElementById("imie").value;
-    imie = imie.replace(/\s/g, '');
-    var regex = /^[A-Za-zÀ-ÖØ-öø-ÿ]+$/;
-    var specjalneznaki = !regex.test(imie);
-    
-    var imie=document.getElementById("imie").value
-    if (imie=="") document.getElementById("komunikat1").innerHTML="Podaj imię" 
-    else if (specjalneznaki) {
-        document.getElementById("komunikat1").innerHTML = "Imię nie może zawierać znaków specjalnych";
+    if (wartosc === '') {
+      document.getElementById(idKomunikatu).innerHTML = `Pole jest wymagane.`;
+    } else {
+      document.getElementById(idKomunikatu).innerHTML = '';
     }
-    else document.getElementById("komunikat1").innerHTML="";
-}
-function sprawdz2() {
-    var nazwisko=document.getElementById("nazwisko").value
-    if (nazwisko=="") document.getElementById("komunikat2").innerHTML="Podaj nazwisko" 
-    else document.getElementById("komunikat2").innerHTML="";
-}
-function sprawdz3() {
-    var email=document.getElementById("email").value
-    if (email=="") document.getElementById("komunikat3").innerHTML="Podaj email" 
-    else document.getElementById("komunikat3").innerHTML="";
-}
-function sprawdz4() {
-    var telefonu=document.getElementById("telefonu").value
-    if (telefonu=="") document.getElementById("komunikat4").innerHTML="Podaj telefonu" 
-    else document.getElementById("komunikat4").innerHTML="";
-}
-function sprawdz5() {
-    var data=document.getElementById("data").value
-    if (data=="") document.getElementById("komunikat5").innerHTML="Podaj date" 
-    else document.getElementById("komunikat5").innerHTML="";
-}
-function sprawdz7() {
-    var miejscowosc=document.getElementById("miejscowosc").value
-    if (miejscowosc=="") document.getElementById("komunikat7").innerHTML="Podaj miejscowość" 
-    else document.getElementById("komunikat7").innerHTML="";
-}
-function sprawdz8() {
-    var ulica=document.getElementById("ulica").value
-    if (ulica=="") document.getElementById("komunikat8").innerHTML="Podaj ulice" 
-    else document.getElementById("komunikat8").innerHTML="";
+  }
+  
+  function sprawdzPakiet() {
+    const pakiety = document.getElementsByName("pakiet");
+    const czyWybrano = Array.from(pakiety).some(pakiet => pakiet.checked);
+    document.getElementById("komunikat_pakiet").innerHTML = czyWybrano ? '' : 'Wybierz pakiet';
+  }
+  function sprawdzBoxa(idBoxa,idKomunikatu) {
+    const checkbox =document.getElementById(idBoxa)
+    if (checkbox.checked) {
+        document.getElementById(idKomunikatu).innerHTML = '';
+    }else{
+        document.getElementById(idKomunikatu).innerHTML = `Pole jest wymagane.`;
+    }
     
-}
-function sprawdz9() {
-    var budynek=document.getElementById("budynek").value
-    if (budynek=="") document.getElementById("komunikat9").innerHTML="Podaj numer budynku " 
-    else document.getElementById("komunikat9").innerHTML="";
-}
-function sprawdz10() {
-    var kod=document.getElementById("kod").value
-    if (kod=="") document.getElementById("komunikat10").innerHTML="Podaj kod pocztowy" 
-    else document.getElementById("komunikat10").innerHTML="";
-}
+  }
+  function sprawdzWszystko() {
+    sprawdzPole('imie', 'komunikat_imie');
+    sprawdzPole('nazwisko', 'komunikat_Nazwisko');
+    sprawdzPole('email','komunikat_mail')
+    sprawdzPole('telefonu','komunikat_Tel')
+    sprawdzPole('telefonu','komunikat_Miejscowosc')
+    sprawdzPole('ulica','komunikat_ulica')
+    sprawdzPole('budynek','komunikat_budynek')
+    sprawdzPole('kod','komunikat_kod')
+    sprawdzPole('zainteresowania','komunikat_zainteresowania')
+    sprawdzBoxa('regulamin','komunikat_regulamin')
+    sprawdzBoxa('zgoda','komunikat_dane_osobowe')
+    sprawdzBoxa('Ezgoda','komunikat_faktury')
+    sprawdzPakiet();
+  }
 
 
-
-
-function sprawdzALL() {
-    var imie=document.getElementById("imie").value
-    if (imie=="") document.getElementById("komunikat1").innerHTML="Podaj imię" 
-    else document.getElementById("komunikat1").innerHTML="";
-    
-    var nazwisko=document.getElementById("nazwisko").value
-    if (nazwisko=="") document.getElementById("komunikat2").innerHTML="Podaj nazwisko" 
-    else document.getElementById("komunikat2").innerHTML="";
-    
-    var email=document.getElementById("email").value
-    if (email=="") document.getElementById("komunikat3").innerHTML="Podaj email" 
-    else document.getElementById("komunikat3").innerHTML="";
-
-    var telefonu=document.getElementById("telefonu").value
-    if (telefonu=="") document.getElementById("komunikat4").innerHTML="Podaj telefonu" 
-    else document.getElementById("komunikat4").innerHTML="";
-    
-    var data=document.getElementById("data").value
-    if (data=="") document.getElementById("komunikat5").innerHTML="Podaj date" 
-    else document.getElementById("komunikat5").innerHTML="";
-    /////
-    var pakiet=document.getElementsByName("pakiet")
-    let czyWybrano = false;
-    for (let i = 0; i < pakiet.length; i++) {
-        if (pakiet[i].checked) {
-          czyWybrano = true;
-          break; 
-        }
-      }
-      if (!czyWybrano) document.getElementById("komunikat6").innerHTML="Wybierz pakiet" 
-      else document.getElementById("komunikat6").innerHTML="";
-    /////
-    var miejscowosc=document.getElementById("miejscowosc").value
-    if (miejscowosc=="") document.getElementById("komunikat7").innerHTML="Podaj miejscowosc" 
-    else document.getElementById("komunikat7").innerHTML="";
-
-    var ulica=document.getElementById("ulica").value
-    if (ulica=="") document.getElementById("komunikat8").innerHTML="Podaj ulice" 
-    else document.getElementById("komunikat8").innerHTML="";
-    
-    var budynek=document.getElementById("budynek").value
-    if (budynek=="") document.getElementById("komunikat9").innerHTML="Podaj numer budynku" 
-    else document.getElementById("komunikat9").innerHTML="";
-
-    var kod=document.getElementById("kod").value
-    if (kod=="") document.getElementById("komunikat10").innerHTML="Podaj kod pocztowy" 
-    else document.getElementById("komunikat10").innerHTML="";
-      
-}
--- strona.php
+++ strona.php
@@ -11,30 +11,28 @@
     <div id="container">
         <form id="dane">
             <label for="imie">Imię</label>
-            <span id="komunikat1"></span>
-            <input name="imie" id="imie" placeholder="Wprowadź imię" type="text" oninput="sprawdz1()">
-            
-            
-            
+            <span id="komunikat_imie"></span>
+            <input name="imie" id="imie" placeholder="Wprowadź imię" type="text" oninput="sprawdzPole('imie','komunikat_imie')">
             
             
             <label for="nazwisko">Nazwisko</label>
-            <span id="komunikat2"></span>
-            <input name="nazwisko" id="nazwisko" placeholder="Wprowadź nazwisko" type="text" oninput="sprawdz2()">
+            <span id="komunikat_Nazwisko"></span>
+            <input name="nazwisko" id="nazwisko" placeholder="Wprowadź nazwisko" type="text" oninput="sprawdzPole('nazwisko','komunikat_Nazwisko')">
             
 
             <label for="e-mail">Adres e-mail</label>
-            <span id="komunikat3"></span>
-            <input type="email" id="email" name="e-mail" placeholder="np. example@ex.com" oninput="sprawdz3()">
+            <span id="komunikat_mail"></span>
+            <input type="email" id="email" name="e-mail" placeholder="np. example@ex.com" oninput="sprawdzPole('email','komunikat_mail')">
             
 
+
             <label for="telefonu">Numer telefonu</label>
-            <span id="komunikat4"></span>
-            <input type="text" name="telefonu" id="telefonu" placeholder="np. 964367387" oninput="sprawdz4()">
+            <span id="komunikat_Tel"></span>
+            <input type="text" name="telefonu" id="telefonu" placeholder="np. 964367387" oninput="sprawdzPole('telefonu','komunikat_Tel')" pattern="[0-9]{9}">
             
 
             <label for="data">Planowany dzień rozpoczęcia korzystania z przestrzeni</label>
-            <span id="komunikat5"></span>
+            <span id="komunikat_data"></span>
             <script>
                 function setMinDate() {
                 const today = new Date();
@@ -47,11 +45,11 @@
                 }   
                 window.onload = setMinDate;
             </script>
-            <input type="date" name="data" id="data" oninput="sprawdz5()">
+            <input type="date" name="data" id="data" oninput="sprawdzPole('data','komunikat_data')">
             
 
             <label>Wybierz pakiet:</label>
-            <span id="komunikat6"></span>
+            <span id="komunikat_pakiet"></span>
             <label for="coworker">COWORKER (50zł)</label>
             <input type="radio" name="pakiet" id="coworker" value="coworker">
             
@@ -60,42 +58,51 @@
             
             
             <label for="miejscowość">Adres korespondencyjny - Miejscowość</label>
-            <span id="komunikat7"></span>
-            <input type="text" name="miejscowość" placeholder="Wprowadź miejscowość korespondencyjną" id="miejscowosc" oninput="sprawdz7()">
+            <span id="komunikat_Miejscowosc"></span>
+            <input type="text" name="miejscowosc" placeholder="Wprowadź miejscowość korespondencyjną" id="miejscowosc" oninput="sprawdzPole('miejscowosc','komunikat_Miejscowosc')">
 
             <label for="ulica">Adres korespondencyjny - Ulica</label>
-            <span id="komunikat8"></span>
-            <input type="text" name="ulica" placeholder="Wprowadź ulicę" id="ulica" oninput="sprawdz8()">
+            <span id="komunikat_ulica"></span>
+            <input type="text" name="ulica" placeholder="Wprowadź ulicę" id="ulica" oninput="sprawdzPole('ulica','komunikat_ulica')">
             
             <label for="numer">Adres korespondencyjny - Numer budynku</label>
-            <span id="komunikat9"></span>
-            <input type="text" name="budynek" placeholder="Wprowadź numer budynku" id="budynek" oninput="sprawdz9()">
+            <span id="komunikat_budynek"></span>
+            <input type="text" name="budynek" placeholder="Wprowadź numer budynku" id="budynek" oninput="sprawdzPole('budynek','komunikat_budynek')">
 
 
             <label for="numer">Adres korespondencyjny - Numer mieszkania</label>
             <input type="text" name="numer" placeholder="Wprowadź numer mieszkania" id="mieszkanie">
 
             <label for="kod">Adres korespondencyjny - kod pocztowy</label>
-            <span id="komunikat10"></span>
-            <input type="text" name="kod" placeholder="Wprowadź kod pocztowy" oninput="sprawdz10()" id="kod">
+            <span id="komunikat_kod"></span>
+            <input type="text" name="kod" placeholder="Wprowadź kod pocztowy" oninput="sprawdzPole('kod','komunikat_kod')" id="kod" >
 
             <label for="zainteresowania">Moje zainteresowania/kompetencje</label>
-            <span id="komunikat10"></span>
-            <textarea name="zainteresowania" id="zainteresowania" oninput="sprawdz11()"></textarea>
+            <span id="komunikat_zainteresowania"></span>
+            <textarea name="zainteresowania" id="zainteresowania" oninput="sprawdzPole('zainteresowania','komunikat_zainteresowania')"></textarea>
 
             <h3>Zapoznanie z Regulaminem</h3>
+            <span id="komunikat_regulamin"></span>
             <input type="checkbox" name="Regulamin" id="regulamin">
             <label for="regulamin">Oświadczam, że zapoznałem/am się z Regulaminem i zobowiązuję się do jego przestrzegania.</label>
 
             <h3>Zgoda na przetwarzanie i przechowywanie danych osobowych</h3>
+            <span id="komunikat_dane_osobowe"></span>
             <input type="checkbox" name="zgoda" id="zgoda">
             <label for="zgoda">Wyrażam zgodę na przetwarzanie moich danych osobowych przez ICHB PAN PCSS.</label>
 
             <h3>Zgoda na przesyłanie faktur/rachunków drogą elektroniczną</h3>
-            <input type="checkbox" name="EZgoda" id="EZgoda">
+            <span id="komunikat_faktury"></span>
+            <input type="checkbox" name="Ezgoda" id="Ezgoda">
             <label for="EZgoda">Wyrażam zgodę na przesyłanie faktur drogą elektroniczną.</label>
 
-            <input type="button" value="WYŚLIJ" onclick="sprawdzALL()">
+
+            <h3>Jestem zainteresowany otrzymywaniem ofert współpracy adekwatnie do wskazanych zainteresowan/kompetencji nie częściej niż raz w tygodniu</h3>
+            <span id="komunikat_faktury"></span>
+            <input type="checkbox" name="Ezgoda" id="Ezgoda">
+            
+
+            <input type="button" value="WYŚLIJ" onclick="sprawdzWszystko()">
         </form>
     </div>
 </body>
